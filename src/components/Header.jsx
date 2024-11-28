c1e69ff60d40e66c4f68c863f84b8dfab0f92964
jeffry.miranda <jeffry.miranda@daugherty.cr>
2024-11-28 16:04:55 -0600

    Styling react apps with inline styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,14 @@ import logo from '../assets/logo.png';
 import './Header.css'
 
 export default function Header() {
-  return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </header>
-  );
+    return (
+        <header>
+            <img src={logo} alt="A canvas"/>
+            <h1>ReactArt</h1>
+            <p style={{
+                color: 'red',
+                textAlign: 'left'
+            }}>A community of artists and art-lovers.</p>
+        </header>
+    );
 }
